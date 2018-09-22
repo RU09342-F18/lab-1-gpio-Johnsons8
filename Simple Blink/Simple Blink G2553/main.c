d7ce7279242dad30a14a4288d5037d61fc62bc7a
Johnsons8 <35497793+Johnsons8@users.noreply.github.com>
2018-09-21 22:39:23 -0400

    Comments added

--- Simple Blink/Simple Blink G2553/main.c
+++ Simple Blink/Simple Blink G2553/main.c
@@ -1,20 +1,16 @@
 #include <msp430.h> 
-#include <msp430G2553.h>
-
-/*
- * main.c
- */
+#include <msp430G2553.h>                    //This board specifically
 
 void main(void)
 {
     WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
-     P1DIR |= 0x01;                            // Set P1.0 to output direction
-     for(;;)
+     P1DIR |= 0x01;                            // Tells P1 to be an output
+     for(;;)                                   //Infinite loop
      {
-       volatile unsigned int i;
-         P1OUT ^= 0x01;                        // if P1.4 set, set P1.0
-      i = 20000;
-      do(i--);
+       volatile unsigned int i;                 // makes a variable, unsigned so it can be larger and it is not expected to go negative
+         P1OUT ^= 0x01;                        // Toggles LED using XOR statement
+      i = 20000;                                // resetting i to 200000, which is how many cycles of do while loop will run before next reset
+      do(i--);                                  // beginning of do while loop, incrementing i to be 1 less each time
       while(i != 0);
      }
-} // CCR0_
+}_
