66e8d200b3a2cab983807686c11165dc6513ccb0
Johnsons8 <35497793+Johnsons8@users.noreply.github.com>
2018-09-21 22:45:06 -0400

    Update main.c

--- Multiple Blink/Multiple Blink MSP430G2553/main.c
+++ Multiple Blink/Multiple Blink MSP430G2553/main.c
@@ -9,30 +9,21 @@ void main(void)
 {
     WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
      P1DIR |= BIT0;                            // Set P1.0 to output direction
-     P1DIR |= BIT6;
-     for(;;)
+     P1DIR |= BIT6;                            // Set P1.6 to output direction
+     for(;;)                                    // infinite loop
      {
        volatile unsigned int i;
          i = 350000;
-
-
-      while(i != 0)
+     while(i != 0)                          
       {
        i--;
-       if( i % 5000 == 0 ) {
+       if( i % 5000 == 0 ) {                    //Everytime counter goes down to something divisible by 5000 LED1 toggles
             P1OUT ^= BIT0;
          }
-         if( i % 7000 == 0 ){
+         if( i % 7000 == 0 ){                   //Everytime counter goes down to something divisible by 7000 LED2 toggles
              P1OUT ^= BIT6;
          }
-
       }
-
-
-
-
-
      }
-
 }
 
